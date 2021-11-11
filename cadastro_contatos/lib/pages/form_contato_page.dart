b387887f965a069b6767466e0977f05a6264ed34
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2021-11-11 20:23:47 -0300

    Obter imagem da galeria

--- cadastro_contatos/lib/pages/form_contato_page.dart
+++ cadastro_contatos/lib/pages/form_contato_page.dart
@@ -2,7 +2,10 @@ import 'package:cadastro_contatos/dao/contato_dao.dart';
 import 'package:cadastro_contatos/model/contato.dart';
 import 'package:cadastro_contatos/widgets/visualizador_imagem.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class FormContatoPage extends StatefulWidget {
   final Contato? contato;
@@ -25,6 +28,8 @@ class _FormContatoPageState extends State<FormContatoPage> {
   );
   var _salvando = false;
   late String _tipoImagem;
+  String? _caminhoImagem;
+  final _picker = ImagePicker();
 
   @override
   void initState() {
@@ -34,6 +39,7 @@ class _FormContatoPageState extends State<FormContatoPage> {
       _telefoneController.text = widget.contato!.telefone ?? '';
       _emailController.text = widget.contato!.email ?? '';
       _tipoImagem = widget.contato!.tipoImagem;
+      _caminhoImagem = widget.contato!.caminhoImagem;
     } else {
       _tipoImagem = Contato.tipoImagemAssets;
     }
@@ -148,7 +154,7 @@ class _FormContatoPageState extends State<FormContatoPage> {
                 if (_tipoImagem == Contato.tipoImagemFile) ...[
                   ElevatedButton(
                     child: const Text('Obter da Galeria'),
-                    onPressed: null,
+                    onPressed: () => _usarImagePicker(ImageSource.gallery),
                   ),
                   ElevatedButton(
                     child: const Text('Usar câmera interna'),
@@ -161,6 +167,7 @@ class _FormContatoPageState extends State<FormContatoPage> {
                 ],
                 VisualizadorImagem(
                   tipoImagem: _tipoImagem,
+                  caminhoImagem: _caminhoImagem,
                   size: constraints.maxWidth,
                 ),
               ],
@@ -180,6 +187,8 @@ class _FormContatoPageState extends State<FormContatoPage> {
       nome: _nomeController.text,
       telefone: _telefoneController.text,
       email: _emailController.text,
+      tipoImagem: _tipoImagem,
+      caminhoImagem: _caminhoImagem,
     );
     setState(() {
       _salvando = true;
@@ -189,5 +198,21 @@ class _FormContatoPageState extends State<FormContatoPage> {
       _salvando = false;
     });
     Navigator.of(context).pop(true);
+  }
+
+  Future<void> _usarImagePicker(ImageSource origem) async {
+    final XFile? arquivo = await _picker.pickImage(source: origem);
+    if (arquivo == null) {
+      return;
+    }
+    _tratarArquivo(arquivo);
+  }
+
+  Future<void> _tratarArquivo(XFile arquivo) async {
+    final diretorioBase = await getApplicationDocumentsDirectory();
+    final idArquivo = Uuid().v1();
+    _caminhoImagem = '${diretorioBase.path}/$idArquivo.jpg';
+    await arquivo.saveTo(_caminhoImagem!);
+    setState(() {});
   }
 }
